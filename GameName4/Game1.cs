d42b6cf7a1bda35d5edfc98b8f1def54446a8530
Sean <shamus252@hotmail.co.uk>
2015-02-09 13:28:27 +0000

    Changed background color

--- GameName4/Game1.cs
+++ GameName4/Game1.cs
@@ -81,7 +81,7 @@ namespace GameName4
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Aqua);
 
             // TODO: Add your drawing code here
 
